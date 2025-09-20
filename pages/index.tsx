cd1afa59260d6c82d4406b2b3b8df002a69aec2c
albs415 <albanyaroyan@gmail.com>
2025-09-20 11:16:17 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <Head>
+        <title>Family Birthday Hub</title>
+        <meta name="description" content="Family birthday tracker and planning hub" />
+      </Head>
+      <h1 className="text-4xl font-bold mb-4">Family Birthday Hub</h1>
+      <p className="text-lg">Welcome to the family birthday hub! This app is under construction.</p>
+    </div>
+  )
+}
